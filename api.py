8689890d7c990d088a684cb19f7c1a471bbbe5f5
esad.dag <esad.dag@teamdefinex.com>
2021-07-08 16:26:09 +0300

    cors

--- api.py
+++ api.py
@@ -1,12 +1,15 @@
 from flask import Flask, request
+from flask_cors import CORS, cross_origin
 from api.main import urun_bazli_filtreleme
 app = Flask(__name__)
+cors = CORS(app)
 
 @app.route('/')
 def hello():
     return 'Hello World!'
 
 @app.route('/film', methods=['GET', 'POST'])
+@cross_origin()
 def method_name():
     result =  urun_bazli_filtreleme(request.get_json()["keyword"])
     json = result.reset_index().to_json(orient='records')
